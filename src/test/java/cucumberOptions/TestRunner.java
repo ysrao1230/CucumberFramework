f1c983e47c9a27c62b13f70435cf7ff5003a8820
ysrao1230 <ysrrao.mt@gmail.com>
2023-03-07 15:51:54 +0530

    html,json,xml reports

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = "StepDefination", stepNotifications = true)
+@CucumberOptions(features = "src/test/java/features", glue = "StepDefination", stepNotifications = true, plugin = {
+		"pretty", "html:target/cucumber.html","json:target/cucumber.json","junit:target/cucumberreport.xml" })
 
 public class TestRunner {
 
