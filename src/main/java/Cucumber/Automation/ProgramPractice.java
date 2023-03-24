0cdabb28b76abc33e7b5753939824aa2d1d9a18c
ysrao1230 <yamalapalli1230@gmail.com>
2023-03-24 23:06:18 +0530

    Programs

--- src/main/java/Cucumber/Automation/ProgramPractice.java
+++ src/main/java/Cucumber/Automation/ProgramPractice.java
@@ -7,11 +7,11 @@ import java.util.Map;
 public class ProgramPractice {
 
 	public static void main(String[] args) {
-		
-		int a[]= {2,3,4,2,3,4,4,4,45,4,4,4,4,4};
-		System.out.println("Majority of elements= "+majorityElementsinArray(a, a.length));
-		
-		Integer m[]= {9,1,3,6,8,0};
+		separatenumberchardigit("yagdg#$%^6253536dFGHJ66473");
+		int a[] = { 2, 3, 4, 2, 3, 4, 4, 4, 45, 4, 4, 4, 4, 4 };
+		System.out.println("Majority of elements= " + majorityElementsinArray(a, a.length));
+
+		Integer m[] = { 9, 1, 3, 6, 8, 0 };
 		missingnumberintheArray(m);
 
 	}
@@ -35,19 +35,39 @@ public class ProgramPractice {
 		}
 		return -1;
 	}
-	
+
 	public static void missingnumberintheArray(Integer a[]) {
-		HashSet<Integer> hashset= new HashSet<Integer>();
-				
-		for(int i:a) {
+		HashSet<Integer> hashset = new HashSet<Integer>();
+
+		for (int i : a) {
 			hashset.add(i);
 		}
-		int n=a.length+1;
-		for(int i=0;i<n;i++) {
-			if(!hashset.contains(i)) {
+		int n = a.length + 1;
+		for (int i = 0; i < n; i++) {
+			if (!hashset.contains(i)) {
 				System.out.println(i);
 			}
 		}
 	}
 
+	public static void separatenumberchardigit(String data) {
+		String number = "";
+		String alph = "";
+		String symbol = "";
+		for (int i = 0; i < data.length(); i++) {
+			char a = data.charAt(i);
+			if (Character.isDigit(a)) {
+				number = number + a;
+			} else if (Character.isLetter(a)) {
+				alph = alph + a;
+			} else {
+				symbol = symbol + a;
+			}
+		}
+		System.out.println("Before Splitting: " + data);
+		System.out.println("After Split: ");
+		System.out.println("Numbers in a String: " + number);
+		System.out.println("Alphabets in a string: " + alph);
+		System.out.println("SPecial characters in a string: " + symbol);
+	}
 }
