51e1954a7d1364eea5dead7c6ceffe4fc7efde1f
ysrao1230 <yamalapalli1230@gmail.com>
2023-04-22 22:46:05 +0530

    Minor CHanges in the practice Concepts

--- src/main/java/Cucumber/Automation/SeleniumPracticeCocepts.java
+++ src/main/java/Cucumber/Automation/SeleniumPracticeCocepts.java
@@ -11,37 +11,51 @@ import java.util.List;
 import java.util.Set;
 
 public class SeleniumPracticeCocepts {
-public static WebDriver driver=null;
-    public static void main(String[] args) {
-clickingontheLinksOfthePage();
-    }
+	public static WebDriver driver = null;
 
-    public static void clickingontheLinksOfthePage(){
-driver = new FirefoxDriver();
+	public static void main(String[] args) {
+		clickingontheLinksOfthePage();
+	}
 
-driver.get("https://onpassive.com");
+	public static void clickingontheLinksOfthePage() {
+		driver = new FirefoxDriver();
 
- List<WebElement> allLinks= driver.findElements(By.tagName("a"));
- int sizeOflinks= allLinks.size();
-        System.out.println("Total number of links on the Page:=> "+sizeOflinks);
+		driver.get("https://onpassive.com");
 
-        for (int i=1;i<=sizeOflinks;i++){
-            String linkText= driver.findElement(By.tagName("a")).getAttribute("href");
-            System.out.println(i+ " "+linkText);
-        }
-        WebElement socialmediaObject=driver.findElement(By.className("footer-social-links"));
-        List<WebElement> socialmedialink=socialmediaObject.findElements(By.tagName("a"));
-        System.out.println("Size of the socialmedia links: "+socialmedialink.size());
-        for(int i=0; i<socialmedialink.size();i++){
-            String keyeventClick= Keys.chord(Keys.CONTROL,Keys.ENTER);
-            socialmedialink.get(i).sendKeys(keyeventClick);
-        }
-        Set<String> allWindow= driver.getWindowHandles();
-        Iterator<String> it= allWindow.iterator();
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+		int sizeOflinks = allLinks.size();
+		System.out.println("Total number of links on the Page:=> " + sizeOflinks);
 
-        while (it.hasNext()){
-            driver.switchTo().window(it.next());
-            System.out.println("Page title is: "+driver.getTitle());
-        }
-    }
+		for (int i = 1; i <= sizeOflinks; i++) {
+			String linkText = driver.findElement(By.tagName("a")).getAttribute("href");
+			System.out.println(i + " " + linkText);
+		}
+		WebElement socialmediaObject = driver.findElement(By.className("footer-social-links"));
+		WebElement headerLinks = driver.findElement(By.xpath("//*[@id='navbarText']/ul"));
+		List<WebElement> socialmedialink = headerLinks.findElements(By.tagName("a"));
+		System.out.println("Size of the socialmedia links: " + socialmedialink.size());
+		for (int i = 0; i < socialmedialink.size(); i++) {
+			String keyeventClick = Keys.chord(Keys.CONTROL, Keys.ENTER);
+			try {
+				socialmedialink.get(i).sendKeys(keyeventClick);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+		}
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Set<String> allWindow = driver.getWindowHandles();
+		Iterator<String> it = allWindow.iterator();
+
+		while (it.hasNext()) {
+			driver.switchTo().window(it.next());
+			System.out.println("Page title is: " + driver.getTitle());
+			System.out.println("data");
+		}
+	}
 }
