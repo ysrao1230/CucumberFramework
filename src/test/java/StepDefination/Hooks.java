199a6e1dc1b97797477cca6eb0fe3cec551a4dff
ysrao1230 <yamalapalli1230@gmail.com>
2023-05-21 15:55:33 +0530

    Hooks method name change

--- src/test/java/StepDefination/Hooks.java
+++ src/test/java/StepDefination/Hooks.java
@@ -19,66 +19,73 @@ import java.io.IOException;
 
 public class Hooks {
 
-	BasePage basePage;
-	LoginPage loginPage;
-	// WebDriver driver;
-	ExtentSparkReporter extent;
-	ExtentReports reports = new ExtentReports();
+    BasePage basePage;
+    LoginPage loginPage;
+    // WebDriver driver;
+    ExtentSparkReporter extent;
+    ExtentReports reports = new ExtentReports();
 
-	public Hooks(BasePage basePage) {
-		this.basePage = basePage;
-		reports = new ExtentReports();
-	}
+    public Hooks(BasePage basePage) {
+        this.basePage = basePage;
+        reports = new ExtentReports();
+    }
 
-	@Before("@APITesting")
-	public void beforeAPI() {
-		System.out.println("Start the API Testing using hooks \r\n");
-	}
+    @Before("@APITesting")
+    public void beforeAPI() {
+        System.out.println("Start the API Testing using hooks \r\n");
+    }
 
-	@After("@APITesting")
-	public void afterPI() {
-		System.out.println("\r\n ENd of the API Testing using hooks ");
-	}
+    @After("@APITesting")
+    public void afterPI() {
+        System.out.println("\r\n ENd of the API Testing using hooks ");
+    }
 
-	@Before("@WebTesting")
-	public void beforewebTesting() {
-		extent = new ExtentSparkReporter("spark_report.html");
-		reports.attachReporter(extent);
-		reports.setSystemInfo("OS", "Windows-10");
-		System.out.println("Start the Web  Testing using hooks \r\n");
-		reports.createTest("Browser Launch").log(Status.INFO, "Launching Browser");
-		basePage.browserIntialization();
-		String browserMessage = "Browser " + basePage.browserName + "Launched successfully";
-		reports.createTest("Browser info").log(Status.WARNING, browserMessage);
-		loginPage = new LoginPage(basePage);
-		loginPage.userNameInput(AppConfig.userName);
-		loginPage.passwordNameInput(AppConfig.passWord);
-		loginPage.loginButtonAction();
-		reports.createTest("Logged In").log(Status.PASS, "Application logged successfully");
-	}
+    @Before("@WebTesting")
+    public void beforewebTesting() {
+        extent = new ExtentSparkReporter("spark_report.html");
+        reports.attachReporter(extent);
+        reports.setSystemInfo("OS", "Windows-10");
+        System.out.println("Start the Web  Testing using hooks \r\n");
+        reports.createTest("Browser Launch").log(Status.INFO, "Launching Browser");
+        basePage.browserIntialization();
+        String browserMessage = "Browser " + basePage.browserName + "Launched successfully";
+        reports.createTest("Browser info").log(Status.WARNING, browserMessage);
+        loginPage = new LoginPage(basePage);
+        loginPage.userNameInput(AppConfig.userName);
+        loginPage.passwordNameInput(AppConfig.passWord);
+        loginPage.loginButtonAction();
+        reports.createTest("Logged In").log(Status.PASS, "Application logged successfully");
+    }
 
-	@After("@WebTesting")
-	public void afterWebTesting() {
-		System.out.println("\r\n ENd of the Web Testing using hooks");
-		try {
-			Thread.sleep(6000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		basePage.driver.quit();
-		reports.flush();
-	}
+    @After("@WebTesting")
+    public void afterWebTesting() {
+        System.out.println("\r\n ENd of the Web Testing using hooks");
+        try {
+            Thread.sleep(6000);
 
-	@AfterStep
-	public void addScreenshotToFailedScenario(Scenario scenario) throws IOException {
-		if (scenario.isFailed()) {
-			// Screenshot capturing
-			File sourcePath = ((TakesScreenshot) basePage.driver).getScreenshotAs(OutputType.FILE);
-			byte[] fileAttach = FileUtils.readFileToByteArray(sourcePath);
-			scenario.attach(fileAttach, "image/png", "image");
-			System.out.println("Capturing the screenshot");
-		}
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        basePage.driver.close();
+        if (basePage.driver != null) {
+            basePage.driver.quit();
+            reports.createTest("Browser closing").log(Status.PASS, "Browser Has been Closed");
+        } else {
+            reports.createTest("Browser value not exist").log(Status.FAIL, "Browser not closed successfully");
+        }
 
-	}
+        reports.flush();
+    }
+
+    @AfterStep
+    public void addScreenshotToFailedScenario(Scenario scenario) throws IOException {
+        if (scenario.isFailed()) {
+            // Screenshot capturing
+            File sourcePath = ((TakesScreenshot) basePage.driver).getScreenshotAs(OutputType.FILE);
+            byte[] fileAttach = FileUtils.readFileToByteArray(sourcePath);
+            scenario.attach(fileAttach, "image/png", "image");
+            System.out.println("Capturing the screenshot");
+        }
+
+    }
 }
